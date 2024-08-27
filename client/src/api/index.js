4bfb87280d549785b30b021b6f28a6bb4fa1c1d0
Shantanu Vedante <helpshantanu03@gmail.com>
2024-08-27 11:17:41 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fooddelivery-mern.onrender.com/api/",
+  baseURL: "https://fooddelivery-mern-pxru.onrender.com/api/",
 });
 
 //auth
